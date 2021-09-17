94ae671d711fd02a4ec75bdec910cc3b7c65e36e
mio256 <nao.reiya0215@gmail.com>
2021-09-17 17:19:00 +0900

    ver 2.0

---
+++ SaveText.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class SaveText {
+
+    private String memoStr;
+
+    public void put(String str) {
+        memoStr = str;
+
+    }
+
+    public void save() {
+        LocalDateTime time = LocalDateTime.now();
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyyMMdd");
+
+        try (FileWriter fw = new FileWriter("/home/skyadmin/Desktop/" + time.format(format) + ".txt")) {
+            fw.write(memoStr);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+    }
+
+}
